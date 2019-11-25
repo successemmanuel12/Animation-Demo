1c933ac5fe331762a1ae7500eb813a44d1227c7b
Success <successemmanuel12@gmail.com>
2019-11-25 15:46:04 +0100

    Transitions without scenes.

--- app/src/main/java/com/success/animationdemo/MainActivity.kt
+++ app/src/main/java/com/success/animationdemo/MainActivity.kt
@@ -1,66 +1,41 @@
 package com.success.animationdemo
 
 import android.os.Bundle
-import android.transition.*
+import android.transition.Fade
+
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
 import android.view.View
-import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import com.success.animationdemo.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-	private lateinit var scene1: Scene
-	private lateinit var scene2: Scene
-	private lateinit var currentScene: Scene
-	private lateinit var transition: Transition
-	private lateinit var transitionSet: TransitionSet
+	private var visibility = false
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		// Step 1: Create a Scene object for both the starting and ending layout
-		scene1 = Scene.getSceneForLayout(sceneRoot, R.layout.scene1, this)
-		scene2 = Scene.getSceneForLayout(sceneRoot, R.layout.scene2, this)
 
-
-		// Step 2: Create a Transition object to define what type of animation you want
-		//transition = TransitionInflater.from(this).inflateTransition(R.transition.example_2)
-
-		val cbTransition = ChangeBounds()
-		cbTransition.duration = 500
-		cbTransition.interpolator = LinearInterpolator()
-
-		val fadeInTransition = Fade(Fade.IN)
-		fadeInTransition.duration = 250
-		fadeInTransition.startDelay = 400
-		fadeInTransition.addTarget(R.id.txvTitle)
-
-		val fadeOutTransition = Fade(Fade.OUT)
-		fadeOutTransition.duration = 50
-		fadeOutTransition.addTarget(R.id.txvTitle)
-
-		transitionSet = TransitionSet()
-		transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
-
-		transitionSet.addTransition(cbTransition)
-		transitionSet.addTransition(fadeInTransition)
-		transitionSet.addTransition(fadeOutTransition)
-
-		scene1.enter()
-		currentScene = scene1
 	}
 
-	fun onClick(view: View) {
+	fun fadeAnimation(view: View) {
+		val transition = Fade()
+		TransitionManager.beginDelayedTransition(sceneRoot, transition)
 
-		// Step 3: Call TransitionManager.go() to run animation
-		if (currentScene === scene1) {
-			TransitionManager.go(scene2, transitionSet)
-			currentScene = scene2
-		}else
-			TransitionManager.go(scene1, transitionSet)
-			currentScene = scene1
+		txvDescription.visibility = if (visibility) View.INVISIBLE else View.VISIBLE
+		visibility = !visibility
+	}
+
+	fun slideEffect(view: View) {
+
+		val transition = Slide(Gravity.END)
+		TransitionManager.beginDelayedTransition(sceneRoot, transition)
+
+		txvDescription.visibility = if (visibility) View.INVISIBLE else View.VISIBLE
+		visibility = !visibility
 	}
 }
